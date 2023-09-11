008142d7aa236731f1a8e4fef882cb1438272f8b
Сергей Политаев <ronin@sonikelf.ru>
2023-09-12 01:55:12 +0300

    add navigation section

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { styled } from 'styled-components';
+
+import Logo from './Logo';
+import MainNav from './MainNav';
+
 const StyledSidebar = styled.aside`
 	background-color: var(--color-grey-0);
 	padding: 3.2rem 2.4rem;
@@ -9,5 +13,10 @@ const StyledSidebar = styled.aside`
 `;
 
 export default function Sidebar() {
-	return <StyledSidebar>sidebar</StyledSidebar>;
+	return (
+		<StyledSidebar>
+			<Logo />
+			<MainNav />
+		</StyledSidebar>
+	);
 }
