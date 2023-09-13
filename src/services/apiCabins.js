60edcbc8263cbaa484bf1f27375accac9da0885d
Сергей Политаев <ronin@sonikelf.ru>
2023-09-13 04:07:35 +0300

    fix cabins fetching

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,7 +1,7 @@
 import supabase from './supabase';
 
 export async function getCabins() {
-	const { data, error } = await supabase.from('cabins'.select('*'));
+	const { data, error } = await supabase.from('cabins').select('*');
 	if (error) {
 		throw new Error('cabins could not be loaded');
 	}
