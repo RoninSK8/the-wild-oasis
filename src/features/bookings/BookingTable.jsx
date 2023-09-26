83d5892e7543ad289517d1d2946dbd2cecd0832d
Сергей Политаев <ronin@sonikelf.ru>
2023-09-26 04:50:12 +0300

    Build pagination component

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -4,6 +4,7 @@ import Table from '../../ui/Table';
 import Menus from '../../ui/Menus';
 import Empty from '../../ui/Empty';
 import Spinner from '../../ui/Spinner';
+import Pagination from '../../ui/Pagination';
 
 function BookingTable() {
 	const { bookings, isLoading } = useBookings();
@@ -34,6 +35,9 @@ function BookingTable() {
 						<BookingRow key={booking.id} booking={booking} />
 					)}
 				/>
+				<Table.Footer>
+					<Pagination count={45} />
+				</Table.Footer>
 			</Table>
 		</Menus>
 	);
