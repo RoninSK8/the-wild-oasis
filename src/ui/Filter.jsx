86b6a698c4572e70601be7bfb67b5168ba8605d5
Сергей Политаев <ronin@sonikelf.ru>
2023-09-26 03:50:30 +0300

    Implement filter option on the bookings page

--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -38,8 +38,9 @@ const FilterButton = styled.button`
 function Filter({ filterField, options }) {
 	const [searchParams, setSearchParams] = useSearchParams();
 	const currentFilter = searchParams.get(filterField) || options.at(0).value;
+
 	function handleClick(value) {
-		searchParams.set('discount', value);
+		searchParams.set(filterField, value);
 		setSearchParams(searchParams);
 	}
 	return (
