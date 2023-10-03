0abaee58b6c46c423822a820bea2b8b30c3f87af
Сергей Политаев <ronin@sonikelf.ru>
2023-10-04 01:08:58 +0300

    Implement updating user data and password

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,4 +1,4 @@
-import supabase from './supabase';
+import supabase, { supabaseUrl } from './supabase';
 
 export async function signup({ fullName, email, password }) {
 	const { data, error } = await supabase.auth.signUp({
@@ -40,4 +40,38 @@ export async function getCurrentUser() {
 export async function logout() {
 	const { error } = await supabase.auth.signOut();
 	if (error) throw new Error(error.message);
+}
+
+export async function updateCurrentUser({ password, fullName, avatar }) {
+	let updateData;
+	if (password) updateData = { password };
+	if (fullName) updateData = { data: { fullName } };
+
+	const { data, error } = await supabase.auth.updateUser(updateData);
+
+	if (error) throw new Error(error.message);
+
+	if (!avatar) return data;
+
+	const fileName = `avatar-${data.user.id}-${Math.random()}`;
+
+	const { error: uploadAvatarError } = await supabase.storage
+		.from('avatars')
+		.upload(fileName, avatar);
+
+	if (uploadAvatarError) {
+		throw new Error(uploadAvatarError.message);
+	}
+
+	const { data: updatedUser, error: updateUserAvatarError } =
+		await supabase.auth.updateUser({
+			data: {
+				avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
+			},
+		});
+
+	if (updateUserAvatarError) {
+		throw new Error(updateUserAvatarError.message);
+	}
+	return updatedUser;
 }
