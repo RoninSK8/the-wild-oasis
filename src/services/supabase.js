0b2d2dfdb6823e9acfc96771380475795e54a70d
Сергей Политаев <ronin@sonikelf.ru>
2025-04-25 06:16:55 +0300

    Update database: restore supabase db from old db backup

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const supabaseUrl = 'https://gojhhxxdelsegeolghsb.supabase.co';
+export const supabaseUrl = 'https://ykouwoefypiwzzazjrss.supabase.co';
 const supabaseKey =
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdvamhoeHhkZWxzZWdlb2xnaHNiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQ0NzQ3NzYsImV4cCI6MjAxMDA1MDc3Nn0.ZnBBGI0niX7wsGQ6nrzwYAiDcuyU5NdkrzKU0yTGisA';
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlrb3V3b2VmeXBpd3p6YXpqcnNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU1NDQwNzYsImV4cCI6MjA2MTEyMDA3Nn0.ksxC1MTARcQnZDni1mlX-tr3iJOQi2iocF1cgbag_f4';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
